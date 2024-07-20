a27e920cce59df8747d20fedde9ebb5218756836
ssidikov <sardorbek.sidikov7@gmail.com>
2024-07-20 11:23:56 +0200

    Debugging: Refactor Dashboard.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,5 @@
-import { ROUTES_PATH } from "../constants/routes.js";
-export let PREVIOUS_LOCATION = "";
+import { ROUTES_PATH } from '../constants/routes.js';
+export let PREVIOUS_LOCATION = '';
 
 // we use a class so as to test its methods in e2e tests
 export default class Login {
@@ -10,45 +10,45 @@ export default class Login {
     this.PREVIOUS_LOCATION = PREVIOUS_LOCATION;
     this.store = store;
     const formEmployee = this.document.querySelector(`form[data-testid="form-employee"]`);
-    formEmployee.addEventListener("submit", this.handleSubmitEmployee);
+    formEmployee.addEventListener('submit', this.handleSubmitEmployee);
     const formAdmin = this.document.querySelector(`form[data-testid="form-admin"]`);
-    formAdmin.addEventListener("submit", this.handleSubmitAdmin);
+    formAdmin.addEventListener('submit', this.handleSubmitAdmin);
   }
   handleSubmitEmployee = (e) => {
     e.preventDefault();
     const user = {
-      type: "Employee",
+      type: 'Employee',
       email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
-      status: "connected",
+      status: 'connected',
     };
-    this.localStorage.setItem("user", JSON.stringify(user));
+    this.localStorage.setItem('user', JSON.stringify(user));
     this.login(user)
       .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH["Bills"]);
-        this.PREVIOUS_LOCATION = ROUTES_PATH["Bills"];
+        this.onNavigate(ROUTES_PATH['Bills']);
+        this.PREVIOUS_LOCATION = ROUTES_PATH['Bills'];
         PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
-        this.document.body.style.backgroundColor = "#fff";
+        this.document.body.style.backgroundColor = '#fff';
       });
   };
 
   handleSubmitAdmin = (e) => {
     e.preventDefault();
     const user = {
-      type: "Admin",
+      type: 'Admin',
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
-      status: "connected",
+      status: 'connected',
     };
-    this.localStorage.setItem("user", JSON.stringify(user));
+    this.localStorage.setItem('user', JSON.stringify(user));
     this.login(user)
       .catch((err) => this.createUser(user))
       .then(() => {
-        this.onNavigate(ROUTES_PATH["Dashboard"]);
-        this.PREVIOUS_LOCATION = ROUTES_PATH["Dashboard"];
+        this.onNavigate(ROUTES_PATH['Dashboard']);
+        this.PREVIOUS_LOCATION = ROUTES_PATH['Dashboard'];
         PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
-        document.body.style.backgroundColor = "#fff";
+        document.body.style.backgroundColor = '#fff';
       });
   };
 
@@ -63,7 +63,7 @@ export default class Login {
           })
         )
         .then(({ jwt }) => {
-          localStorage.setItem("jwt", jwt);
+          localStorage.setItem('jwt', jwt);
         });
     } else {
       return null;
@@ -78,7 +78,7 @@ export default class Login {
         .create({
           data: JSON.stringify({
             type: user.type,
-            name: user.email.split("@")[0],
+            name: user.email.split('@')[0],
             email: user.email,
             password: user.password,
           }),
