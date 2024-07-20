62a97915287e3bbef4d94e5043bbb4015c0a0f78
ssidikov <sardorbek.sidikov7@gmail.com>
2024-07-20 17:11:26 +0200

    Refactoring BillsUI.js

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,7 +1,6 @@
 import VerticalLayout from './VerticalLayout.js';
 import ErrorPage from './ErrorPage.js';
 import LoadingPage from './LoadingPage.js';
-
 import Actions from './Actions.js';
 
 const row = (bill) => {
@@ -20,7 +19,6 @@ const row = (bill) => {
 };
 
 const rows = (data) => {
-  console.log(data);
   if (data && data.length) {
     const sortedBills = data.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedBills.map((bill) => row(bill)).join('');
