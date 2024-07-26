19f35a8d96e3a6105d17a2feceb1982e5d8d0249
ssidikov <sardorbek.sidikov7@gmail.com>
2024-07-26 13:27:24 +0200

    Refactor Login.js for improved code readability

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,22 +2,25 @@
  * @jest-environment jsdom
  */
 
-import '@testing-library/jest-dom';
-import { screen, waitFor } from '@testing-library/dom';
-import BillsUI from '../views/BillsUI.js';
-import { bills } from '../fixtures/bills.js';
-import { ROUTES, ROUTES_PATH } from '../constants/routes.js';
-import { localStorageMock } from '../__mocks__/localStorage.js';
-import Bills from '../containers/Bills.js';
-import mockStore from '../__mocks__/store.js';
-import userEvent from '@testing-library/user-event';
-import router from '../app/Router.js';
+import '@testing-library/jest-dom'; // Import additional Jest matchers for the DOM
+import { screen, waitFor } from '@testing-library/dom'; // Import functions for working with the DOM
+import BillsUI from '../views/BillsUI.js'; // Import Bills user interface
+import { bills } from '../fixtures/bills.js'; // Import test data for bills
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'; // Import routes
+import { localStorageMock } from '../__mocks__/localStorage.js'; // Import mock for localStorage
+import Bills from '../containers/Bills.js'; // Import Bills class
+import mockStore from '../__mocks__/store.js'; // Import mock for store
+import userEvent from '@testing-library/user-event'; // Import for simulating user actions
+import router from '../app/Router.js'; // Import router
 
+// Mock store for testing
 jest.mock('../app/store', () => mockStore);
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on Bills Page', () => {
+    // Test to check if the bill icon in the vertical layout is highlighted
     test('Then bill icon in vertical layout should be highlighted', async () => {
+      // Set localStorage value for user
       Object.defineProperty(window, 'localStorage', { value: localStorageMock });
       window.localStorage.setItem(
         'user',
@@ -25,33 +28,42 @@ describe('Given I am connected as an employee', () => {
           type: 'Employee',
         })
       );
+      // Create and set the root element for routing
       const root = document.createElement('div');
       root.setAttribute('id', 'root');
       document.body.append(root);
+      // Initialize the router and navigate to the bills page
       router();
       window.onNavigate(ROUTES_PATH.Bills);
+      // Wait for the element with test id 'icon-window' to be in the DOM
       await waitFor(() => screen.getByTestId('icon-window'));
+      // Check if the bill icon is highlighted (has the 'active-icon' class)
       const windowIcon = screen.getByTestId('icon-window');
       expect(windowIcon).toHaveClass('active-icon');
     });
 
+    // Test to check if the bills are correctly displayed
     test('Then bills should be ordered from earliest to latest', () => {
+      // Render the BillsUI with test data
       document.body.innerHTML = BillsUI({ data: bills });
+      // Get all date elements and convert them to an array of strings
       const dates = screen
         .getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i)
         .map((a) => a.innerHTML);
+      // Function to sort dates in descending order
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
+      // Check if the dates are sorted correctly
       expect(dates).toEqual(datesSorted);
     });
   });
 });
 
-// Testing the handleClickIconEye function
+// Testing the handleClickIconEye function and the opening of the modal
 describe('Given I am connected as Employee and I am on Bills page', () => {
   describe('When I click on the icon eye', () => {
     test('It should launch the handleClickIconEye function and open the modal', () => {
-      // Mock localStorage
+      // Set localStorage value for user
       Object.defineProperty(window, 'localStorage', { value: localStorageMock });
       window.localStorage.setItem(
         'user',
@@ -68,7 +80,7 @@ describe('Given I am connected as Employee and I am on Bills page', () => {
         document.body.innerHTML = ROUTES({ pathname });
       };
 
-      // Create an instance of Bills
+      // Create an instance of the Bills class
       const billsInit = new Bills({
         document,
         onNavigate,
@@ -79,14 +91,14 @@ describe('Given I am connected as Employee and I am on Bills page', () => {
       // Mock jQuery modal function
       $.fn.modal = jest.fn();
 
-      // Get the first icon eye
+      // Get the first icon eye element
       const eye = screen.getAllByTestId('icon-eye')[0];
 
       // Mock handleClickIconEye function
       const handleClickIconEye = jest.fn(() => billsInit.handleClickIconEye(eye));
       eye.addEventListener('click', handleClickIconEye);
 
-      // Simulate a click on the icon
+      // Simulate a click on the icon eye
       userEvent.click(eye);
 
       // Assertions
@@ -100,6 +112,7 @@ describe('Given I am connected as Employee and I am on Bills page', () => {
 describe('Given I am a user connected as Employee', () => {
   describe('When I click on the button to create a new bill', () => {
     test('It should render NewBill page', () => {
+      // Define navigation function
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
@@ -107,7 +120,7 @@ describe('Given I am a user connected as Employee', () => {
       // Render the BillsUI with test data
       document.body.innerHTML = BillsUI({ data: bills });
 
-      // Create an instance of Bills
+      // Create an instance of the Bills class
       const billsUI = new Bills({
         document,
         onNavigate,
@@ -118,11 +131,11 @@ describe('Given I am a user connected as Employee', () => {
       // Mock handleClickNewBill function
       const handleClickNewBill = jest.fn(billsUI.handleClickNewBill);
 
-      // Get the new bill button
+      // Get the button to create a new bill
       const btnNewBill = screen.getByTestId('btn-new-bill');
       btnNewBill.addEventListener('click', handleClickNewBill);
 
-      // Simulate a click on the button
+      // Simulate a click on the button to create a new bill
       userEvent.click(btnNewBill);
 
       // Assertions
@@ -132,9 +145,10 @@ describe('Given I am a user connected as Employee', () => {
   });
 });
 
-// Integration test GET
+// Integration test for GET requests on the Bills page
 describe('Given I am a user connected as Employee', () => {
   describe('When I am on Bills Page', () => {
+    // Test to check if the bills are correctly fetched from the API
     test('fetches bills from mock API GET', async () => {
       localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'a@a' }));
       const root = document.createElement('div');
@@ -145,7 +159,7 @@ describe('Given I am a user connected as Employee', () => {
       await waitFor(() => screen.getByText('Mes notes de frais'));
       expect(screen.getByText('Mes notes de frais')).toBeTruthy();
     });
-
+    // Handling errors when making API requests
     describe('When an error occurs on API', () => {
       beforeEach(() => {
         jest.spyOn(mockStore, 'bills');
@@ -162,7 +176,7 @@ describe('Given I am a user connected as Employee', () => {
         document.body.appendChild(root);
         router();
       });
-
+      // Test for handling 404 error when fetching bills from API
       test('fetches bills from an API and fails with 404 message error', async () => {
         mockStore.bills.mockImplementationOnce(() => {
           return {
@@ -174,7 +188,7 @@ describe('Given I am a user connected as Employee', () => {
         const message = await screen.getByText(/Erreur 404/);
         expect(message).toBeTruthy();
       });
-
+      // Test for handling 500 error when fetching bills from API
       test('fetches bills from an API and fails with 500 message error', async () => {
         mockStore.bills.mockImplementationOnce(() => {
           return {
